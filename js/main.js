3980e4466b41959bc5257c84d273cdf8e6b15fce
detrox20 <detrox2020@gmail.com>
2020-03-25 10:59:41 +0900

    공지사항 및 자료실 디자인 완성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var menuItems = $(".menu-item");
 var menuLinks = $(".menu-link");
 var subLinks = $(".submenu a");
+var sections = $(".board section");
 var boardItems = $(".board li");
+var tab = $(".tab");
 
 subLinks.attr("class", "icon-dot-circled");
 boardItems.attr("class", "icon-dot-circled");
@@ -12,4 +14,13 @@ menuLinks.click(function(e) {
   $(this)
     .parent()
     .addClass("menu-item-act");
+});
+
+tab.click(function(e) {
+  e.preventDefault();
+  sections.removeClass("board-act");
+  $(this)
+    .parent()
+    .parent()
+    .addClass("board-act");
 });
